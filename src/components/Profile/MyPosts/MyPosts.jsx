be5c15ea7d6fe52436c8f8111199e1c6d8178173
Faviun <lightbringerlal@gmail.com>
2023-04-04 10:56:50 +0300

    lesson 33

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,8 +8,8 @@ const MyPosts = (props) => {
     let addPost =  () => {
         let text = newPostElement.current.value;
         props.addPost(text);
+        newPostElement.current.value = '';
     }
-
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './Redux/State'
-import {addPost} from './Redux/State'
-import {BrowserRouter} from "react-router-dom";
+import {rerenderEntireTree} from "./render";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-    <App state={state} addPost={addPost}/>
-      </BrowserRouter>
-  </React.StrictMode>
-);
+rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
