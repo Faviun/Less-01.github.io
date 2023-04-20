a53ae46321ffa5d1eeeef3f16fa76888e61f7414
Faviun <lightbringerlal@gmail.com>
2023-04-20 16:47:28 +0300

    lesson 42

--- src/Redux/DialogsReducer.js
+++ src/Redux/DialogsReducer.js
@@ -1,7 +1,27 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Loh1'},
+        {id: 2, name: 'Loh2'},
+        {id: 3, name: 'Loh3'},
+        {id: 4, name: 'Loh4'},
+        {id: 5, name: 'Loh5'},
+        {id: 6, name: 'Loh6'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Hi123'},
+        {id: 3, message: 'Hi54'},
+        {id: 4, message: 'Hi45'},
+        {id: 5, message: 'Hi87'}
+    ],
+    newMessageBody: ''
+};
+
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import reportWebVitals from './reportWebVitals';
-import state from './Redux/State';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import store from "./Redux/State";
+import store from "./Redux/ReduxStore";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -26,7 +25,10 @@ let rerenderEntireTree =() => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe( () => {
+    let state = store.getState()
+    rerenderEntireTree(state);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
